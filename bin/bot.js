22c27c7c0564ea841706895fb409779be142b6fa
Gonzalo Zunino <gonzalo.zunino@globant.com>
2016-12-07 02:11:53 -0300

    Changing packages for bot

--- bin/bot.js
+++ bin/bot.js
@@ -8,23 +8,17 @@
  */
 
 var GrandpaBot = require('../lib/grandpabot');
+var Botkit = require('botkit');
 
-/**
- * Environment variables used to configure the bot:
- *
- *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
- *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
- *  BOT_DB_PATH: the path of the SQLite database used by the bot
- *  BOT_NAME: the username you want to give to the bot within your organisation.
- */
-var token = process.env.BOT_API_KEY || require('../token');
-var dbPath = process.env.BOT_DB_PATH;
-var name = process.env.BOT_NAME;
-
-var grandpabot = new GrandpaBot({
-    token: token,
-    dbPath: dbPath,
-    name: name
+var controller = Botkit.slackbot({
+    debug: false
+    //include "log: false" to disable logging
+    //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
+controller.spawn({
+        token: process.env.BOT_API_KEY || require('../token')
+}).startRTM();
+
+var grandpabot = new GrandpaBot(controller);
 grandpabot.run();
