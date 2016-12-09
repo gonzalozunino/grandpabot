f7ccb7529ac318b84a93c961bf89fe643e0b14c4
Gonzalo Zunino <gonzalo.zunino@globant.com>
2016-12-09 01:08:46 -0300

    Updating dm conversation

--- bin/bot.js
+++ bin/bot.js
@@ -38,3 +38,39 @@ controller.hears(['grandpabot', 'grandpa', 'kk', 'jovie', 'viejo'],
 
         bot.reply(message, reply);
     });
+
+controller.hears(['Deja de spamear'],
+    ['direct_mention', 'mention'],
+    function(bot,message) {
+        bot.startConversation(message,function(err, convo) {
+            var userID = message.user;
+            var user = '<@'+userID+'>';
+
+            convo.say('Dale, ' + user + ' lo hablamos por privado entonces...');
+        });
+
+        var lookForTrouble = function(response, dm) {
+            dm.ask('Muy sensato lo del mensaje privado. En qué estarías trabajando ahora?', function(response, dm) {
+                dm.say('Ok, no hay problema.');
+                askHelp(response, dm);
+                dm.next();
+            });
+        };
+
+        var askHelp = function(response, dm) {
+            dm.ask('Ejem... **Glup**. Mandaste el mail a API?', function(response, dm) {
+                dm.say('... bueno de cualquier manera, recordá siempre ponerme en copia por favor.');
+                sayGoodbye(response, dm);
+                dm.next();
+            });
+        };
+
+        var sayGoodbye = function(response, dm) {
+            dm.ask('Otra consulta, estaría disponible para trabajar este Domingo?', function(response, dm) {
+                dm.say('Ok, no importa, lo vemos despues. Disculpame, te tengo que dejar, tengo que mandar un fax a la gente de España, además tengo call en 5. Saludos');
+                dm.next();
+            });
+        };
+
+        bot.startPrivateConversation(message, lookForTrouble);
+    });
